91e1637a2c529ba6fa63096fda1c0f168f00ca8e
Abbas Mehrbanian <abbas.mrbn@gmail.com>
2023-05-27 00:25:19 +0330

    Added ConvNet example, Added weighted pooling

--- convNet.py
+++ convNet.py
@@ -12,6 +12,8 @@ class convNet:
         pooling_size: int,
         stride_pooling: int,
         act_f: activation_func,
+        pooling_params: list = None,
+        decimal_point : int = 2
     ):
         """
         Convolutional Neural Network
@@ -21,25 +23,32 @@ class convNet:
         self.stride_conv = stride_conv
         self.pooling_type = pooling_type
         self.pooling_size = pooling_size
+        self.pooling_params = pooling_params
         self.stride_pooling = stride_pooling
         self.act_f = act_f
+        self.dp = decimal_point
 
     def conv(self, x: np.ndarray) -> np.ndarray:
         conv_output = []
         for j in range(
             0, len(x) - len(self.conv_filter) + 1, self.stride_conv
         ):
-            conv_output.append(
-                np.dot(x[j : j + len(self.conv_filter)], self.conv_filter)
-            )
+            z_in_j = np.round(np.dot(x[j : j + len(self.conv_filter)], self.conv_filter), self.dp)
+            print(f"Z_in({j+1}) = {z_in_j}")
+            conv_output.append(z_in_j)
         return np.array(conv_output)
 
     def forward(self, x):
         out_conv = self.conv(x)
-        out_H = self.act_f(out_conv)
-        out_O = self.pooling_type(
-            out_H, self.pooling_size, self.stride_pooling
-        )
+        print(f"Z_in = {out_conv}")
+        out_H = np.round(self.act_f(out_conv), self.dp)
+        print("After applying hidden layer activation function:")
+        print(f"Z = {out_H}")
+        out_O = np.round(self.pooling_type(
+            out_H, self.pooling_size, self.stride_pooling, self.pooling_params
+        ), self.dp)
+        print("Output after pooling:")
+        print(f"y = {out_O}")
         return out_H, out_O
 
 
@@ -62,4 +71,4 @@ def example():
     print(f"out_H: {np.round(out_H,4)}\nout_O: {np.round(out_O,4)}")
 
 
-example()
+# example()
